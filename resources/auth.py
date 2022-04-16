c5ee4b8f79f02f564d7e4353797bc6c8d97c46bb
DomFo <dominik.fohrmann@gmail.com>
2022-04-16 02:02:38 +0200

    changes req parser to query string

--- resources/auth.py
+++ resources/auth.py
@@ -7,21 +7,24 @@ from models.item import ItemModel
 
 class Auth(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('state',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank")
-    parser.add_argument('code',
-                        type=str,
-                        required=True,
-                        help="Every item needs a store id")
-    parser.add_argument('scope',
-                        type=str,
-                        required=True,
-                        help="Every item needs a store id")
+    parser.add_argument('state', type=str, location='args')
+    parser.add_argument('code', type=str, location='args')
+    parser.add_argument('scope', type=str, location='args')
+
+    # parser.add_argument('state',
+    #                     type=str,
+    #                     required=True,
+    #                     help="This field cannot be left blank")
+    # parser.add_argument('code',
+    #                     type=str,
+    #                     required=True,
+    #                     help="Every item needs a store id")
+    # parser.add_argument('scope',
+    #                     type=str,
+    #                     required=True,
+    #                     help="Every item needs a store id")
 
     def get(self):
         data = Auth.parser.parse_args()
-        print(data)
-        return render_template('hello.html')
+        return render_template('hello.html', code=data['code'])
 
